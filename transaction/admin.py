d01cca6831ba64af049808b004f4cc1acace882e
bigmacamaca <cjmmacalua@gmail.com>
2023-10-02 15:15:47 +0800

    Added transaction admin view

--- transaction/admin.py
+++ transaction/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from transaction.models import Transaction
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+                    'id',
+                    'number_identifier',
+                    'transaction_item',
+                    'transaction_buyer',
+                    'transaction_quantity',
+                    'subtotal',
+                    'checkout_date')
+
+admin.site.register(Transaction, TransactionAdmin)
 
 # Register your models here.
